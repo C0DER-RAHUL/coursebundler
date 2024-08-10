ee05c57a64fd95c76980b2e1a0ee38ad832db285
rahulyadav <98417474+C0DER-RAHUL@users.noreply.github.com>
2024-08-11 00:08:49 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,8 @@ import { catchAsyncError } from "./catchAsyncError.js";
 import { User } from "../models/User.js";
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+  // const { token } = req.cookies;
+    const { token } =  req.user._id;
 
   if (!token) return next(new ErrorHandler("Not Logged In", 401));
 
