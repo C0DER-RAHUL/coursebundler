dab5234acc59ab5426572a9c08ae540537b91251
rahulyadav <98417474+C0DER-RAHUL@users.noreply.github.com>
2024-08-10 13:46:19 +0530

    Update auth.js
    to check

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,18 @@ import { catchAsyncError } from "./catchAsyncError.js";
 import { User } from "../models/User.js";
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+  // const { token } = req.cookies;
 
-  if (!token) return next(new ErrorHandler("Not Logged In", 401));
+  // if (!token) return next(new ErrorHandler("Not Logged In", 401));
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  req.user = await User.findById(decoded._id);
+  // req.user = await User.findById(decoded._id);
+
+  // next();
+
+  
+  req.user = await User.findById(_id);
 
   next();
 });
